6d0cba1d8ed726fb04ba54f487ede8f0ea6209fd
Jean Paul Ruiz <jruiz@activecampaign.com>
2025-10-14 12:53:03 -0400

    Adding config and output folder for OCR process with Google Vision

---
+++ config/train_ocr_vision_char.py
@@ -0,0 +1,37 @@
+# train a character-level model on OCR extracted text
+# dataset from Google Cloud Vision OCR results
+
+out_dir = 'out-ocr-vision-char'
+eval_interval = 250 # keep frequent because we'll overfit
+eval_iters = 200
+log_interval = 10 # don't print too too often
+
+# we expect to overfit on this dataset, so only save when val improves
+always_save_checkpoint = False
+
+wandb_log = False # override via command line if you like
+wandb_project = 'ocr-vision-char'
+wandb_run_name = 'mini-gpt'
+
+dataset = 'ocr_google_vision_pdf'
+gradient_accumulation_steps = 1
+batch_size = 64
+block_size = 256 # context of up to 256 previous characters
+
+# slightly larger model due to bigger vocabulary (190 vs 65 chars)
+n_layer = 6
+n_head = 6
+n_embd = 384
+dropout = 0.2
+
+learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+max_iters = 5000
+lr_decay_iters = 5000 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+
+warmup_iters = 100 # not super necessary potentially
+
+# on macbook also add
+# device = 'cpu'  # run on cpu only
+# compile = False # do not torch compile the model
